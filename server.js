83dab2cb93b45b7b1025f8a26e1a8ab6d98133ef
Jim Horton <38473243+jlhorton72@users.noreply.github.com>
2019-04-08 13:14:48 -0500

    first up load

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const people = require('./people.json');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Homepage',
+    people: people.profiles
+  });
+});
+
+const server = app.listen(7000, () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
