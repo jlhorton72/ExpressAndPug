8eb14b1bf10ce32d30afd531cdb7c1e95eb35642
Jim Horton <38473243+jlhorton72@users.noreply.github.com>
2019-04-08 13:18:14 -0500

    updated views and server

--- server.js
+++ server.js
@@ -17,3 +17,11 @@ app.get('/', (req, res) => {
 const server = app.listen(7000, () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
+
+app.get('/profile', (req, res) => {
+    const person = people.profiles.find(p => p.id === req.query.id);
+    res.render('profile', {
+      title: `About ${person.firstname} ${person.lastname}`,
+      person,
+    });
+  });
